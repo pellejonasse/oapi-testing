9877d6a7bae5ce472d589c88bde408bb2133c215
pelle <you@example.com>
2026-05-16 14:53:09 +0200

    made first generated-api

---
+++ generate.go
@@ -0,0 +1,5 @@
+package main
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=cfg/types.cfg.yaml openapi_specs/first_endpoint.yml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=cfg/server.cfg.yaml openapi_specs/first_endpoint.yml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=cfg/spec.cfg.yaml openapi_specs/first_endpoint.yml
